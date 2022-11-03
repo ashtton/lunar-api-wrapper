bb544e689e20c98f9d0c60b56342fbe4778099d2
Gleeming <7jashton@gmail.com>
2022-11-02 22:15:46 -0400

    Update cooldown packet

--- src/main/java/me/gleeming/lunar/packet/impl/CooldownPacket.java
+++ src/main/java/me/gleeming/lunar/packet/impl/CooldownPacket.java
@@ -6,16 +6,19 @@ import me.gleeming.lunar.packet.LunarPacket;
 import me.gleeming.lunar.packet.info.PacketInfo;
 import me.gleeming.lunar.staff.StaffModule;
 
+import java.util.concurrent.TimeUnit;
+
 @AllArgsConstructor
 @PacketInfo(id = 3)
 public class CooldownPacket extends LunarPacket {
 
-    private final String message;
-    private final long duration;
+    private final String name;
     private final int icon;
+    private final TimeUnit timeUnit;
+    private final long duration;
 
     @Override
     public LunarBuffer applyValues(LunarBuffer buffer) {
-        return buffer.append(message).append(duration).append(icon);
+        return buffer.append(name).append(timeUnit.toMillis(duration)).append(icon);
     }
 }
